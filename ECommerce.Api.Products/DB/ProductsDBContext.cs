19e278533756291d02069c504ce99e3de168f586
Richard Wisbey <richard.wisbey@tattsgroup.com>
2020-12-02 13:53:45 +1000

    Initial Update

---
+++ ECommerce.Api.Products/DB/ProductsDBContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.DB
+{
+   public class ProductsDBContext
+   {
+   }
+}

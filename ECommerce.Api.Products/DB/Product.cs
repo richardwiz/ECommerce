0766c7047ec54c35ccf7747c469b1ebcb5d4d26f
Richard Wisbey <richard.wisbey@tattsgroup.com>
2020-12-02 15:42:25 +1000

    DB Context

---
+++ ECommerce.Api.Products/DB/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Products.DB
+{
+   public class Product
+   {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public int Price { get; set; }
+      public int Inventory { get; set; }
+
+      public Product()
+      {
+
+      }
+   }
+}
--- ECommerce.Api.Products/DB/ProductsDBContext.cs
+++ ECommerce.Api.Products/DB/ProductsDBContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Api.Products.DB
 {
-   public class ProductsDBContext
+   public class ProductsDBContext : DbContext
    {
+      public DbSet<Product> Products { get; set; }
+
+      public ProductsDBContext(DbContextOptions options) : base(options)
+      {
+
+      }
    }
 }

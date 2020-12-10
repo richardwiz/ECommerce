a6d4385f68b445ac8c3b8c6928b8414bd1ea5d0f
Richard Wisbey <richard.wisbey@tattsgroup.com>
2020-12-11 06:39:50 +1000

    Add xUnit Tests

--- ECommerce.Api.Products/DB/Product.cs
+++ ECommerce.Api.Products/DB/Product.cs
@@ -9,7 +9,7 @@ namespace ECommerce.Api.Products.DB
    {
       public int Id { get; set; }
       public string Name { get; set; }
-      public int Price { get; set; }
+      public decimal Price { get; set; }
       public int Inventory { get; set; }
 
       public Product()
